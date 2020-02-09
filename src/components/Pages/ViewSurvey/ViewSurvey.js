be3dfd3ab15f20c7ddca250c8ef36ab74b361631
Lance Hamilton <lance.hamilton24@gmail.com>
2020-02-09 11:41:49 -0600

    completed routing setup

---
+++ src/components/Pages/ViewSurvey/ViewSurvey.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class ViewSurvey extends Component {
+  render() {
+    return (
+      <div>
+        <h1>View Survey</h1>
+      </div>
+    );
+  }
+}
+
+export default ViewSurvey;
